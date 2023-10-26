0a7f5b14f633896adc7423d069a7e7f1c9ffb9c2
Yogesh <81550052+Yogesh200216@users.noreply.github.com>
2023-10-26 09:52:48 +0530

    Update Image_Transform.py

--- Image_Transform.py
+++ Image_Transform.py
@@ -1,2 +1,11 @@
 import streamlit as st
 import cv2
+# Streamlit app title
+st.title("First Name Printer")
+
+# Input for the first name
+first_name = st.text_input("Enter your first name:")
+
+# Print the first name when a value is entered
+if first_name:
+    st.write(f"Your first name is: {first_name}")
