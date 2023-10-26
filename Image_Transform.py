b69eb2f0fb17c61c9331c1f45ff47a0fe0fda3b2
Yogesh <81550052+Yogesh200216@users.noreply.github.com>
2023-10-26 09:54:20 +0530

    Update Image_Transform.py

--- Image_Transform.py
+++ Image_Transform.py
@@ -1,11 +1,50 @@
 import streamlit as st
 import cv2
-# Streamlit app title
-st.title("First Name Printer")
+import numpy as np
+from PIL import Image
 
-# Input for the first name
-first_name = st.text_input("Enter your first name:")
+# Function to apply grayscale transformation
+def grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Print the first name when a value is entered
-if first_name:
-    st.write(f"Your first name is: {first_name}")
+# Function to apply horizontal flip transformation
+def flip(image):
+    return cv2.flip(image, 1)
+
+# Function to apply invert colors transformation
+def invert_colors(image):
+    return 255 - image  # Invert colors by subtracting each pixel value from 255
+
+# Function to apply blur transformation
+def blur(image):
+    return cv2.GaussianBlur(image, (7, 7), 0)
+
+# Streamlit web app
+st.title("Image Transformation App")
+
+uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+if uploaded_image is not None:
+    image = Image.open(uploaded_image)
+    image = np.array(image)
+
+    st.image(image, caption="Original Image", use_column_width=True)
+
+    # Transformation options
+    st.sidebar.header("Transformations")
+    grayscale_option = st.sidebar.checkbox("Grayscale")
+    flip_option = st.sidebar.checkbox("Flip")
+    invert_colors_option = st.sidebar.checkbox("Invert Colors")
+    blur_option = st.sidebar.checkbox("Blur")
+
+    if st.button("Apply Transformations"):
+        if grayscale_option:
+            image = grayscale(image)
+        if flip_option:
+            image = flip(image)
+        if invert_colors_option:
+            image = invert_colors(image)
+        if blur_option:
+            image = blur(image)
+
+        st.image(image, caption="Transformed Image", use_column_width=True)
